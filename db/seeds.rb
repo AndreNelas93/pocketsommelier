3341e292389c1fe7f8e1f2156f191d6d3c893882
Andre Nelas <andrefnelas@gmail.com>
2023-03-07 12:37:20 +0000

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,23 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
+Wine.destroy_all
+Recipe.destroy_all
 
+Wine.create(
+  name: "Titan of Douro",
+  year: 2017,
+  country: "Portugal",
+  region: "Douro",
+  producer: "Titan of Douro"
+)
+
+puts "wine created"
+
+Recipe.create(
+  title: "Beef Wellington",
+  ingredients: "Beef, Mushrooms, Cured Ham, Puff Pastry, Sauce",
+  instructions: "Cook very well and put to the hoven. Very nice!"
+)
+
+puts "recipe created"
