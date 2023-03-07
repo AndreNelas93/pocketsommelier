62342188a97769ddf3929940759436b7ecc35754
Andre Nelas <andrefnelas@gmail.com>
2023-03-07 12:17:17 +0000

    created routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,19 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "pages#home"
+  root "pages#home"
+  get '/pair_wine', to: 'pages#pair_wine'
+  get '/pair_recipe', to: 'pages#pair_recipe'
+
+  resources :recipes, only: [:index, :show]
+
+  resources :pairings, only: [:create, :update]
+
+  resources :wines, only: [:index, :show]
+
+  get '/profile', to: 'pages#profile'
+  get '/cellar', to: 'pairings#cellar'
+  get '/pairings/wines/:wine_id/recipes', to: 'recipes#paired_with_wine'
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
