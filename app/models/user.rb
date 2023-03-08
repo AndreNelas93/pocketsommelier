94ed9cffba1b7e9d3a8b0fb6d5d6e0f8cea70959
Fabio Ramalho <fabiotrajano.eng@gmail.com>
2023-03-08 08:55:01 +0000

    added devise views

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates :last_name, presence: true
-  validates :first_name, presence: true, uniqueness: { scope: :last_name, message: "Combination of first and last name already been used" }
+  validates :first_name, uniqueness: { scope: :last_name, message: "Combination of first and last name already been used" }
 
   has_many :pairings, dependent: :destroy
   has_many :wines, through: :pairings
