913f7085ae1c17f6b4ac5f2508ba280beefcd1a3
Andre Nelas <andrefnelas@gmail.com>
2023-03-09 13:56:18 +0000

    cloudinary

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,4 +2,6 @@ class Recipe < ApplicationRecord
   has_many :recipe_tags, dependent: :destroy
   has_many :tags, through: :recipe_tags
   has_many :pairings, dependent: :destroy
+
+  has_many_attached :photos
 end
