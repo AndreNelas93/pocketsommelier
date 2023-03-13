75ec4cca8d2c7b84b41f9d931bba01df1a350ac9
Andre Nelas <andrefnelas@gmail.com>
2023-03-13 12:24:09 +0000

    Pairing Show Styling

--- app/controllers/wines_controller.rb
+++ app/controllers/wines_controller.rb
@@ -2,11 +2,11 @@ class WinesController < ApplicationController
   def index
     @recipe = Recipe.find_by(title: params[:recipe])
     @tags = @recipe.tags
+    @pairing = Pairing.new
   end
 
   def show
     @wine = Wine.find(params[:id])
-
     @recipe = Recipe.find(params[:recipe_id]) if params[:recipe_id]
   end
 end
