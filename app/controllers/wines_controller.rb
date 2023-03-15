c8fbaf19907c0b3da3c7dc44a0ea002d0cb083f1
Andre Nelas <andrefnelas@gmail.com>
2023-03-15 16:47:29 +0000

    muita coisa

--- app/controllers/wines_controller.rb
+++ app/controllers/wines_controller.rb
@@ -7,7 +7,7 @@ class WinesController < ApplicationController
     @recipe = Recipe.find_by(title: params[:recipe])
     @tags = @recipe.tags
     @tags.each do |tag|
-      tag.wines.each do |wine|
+      tag.wines.with_attached_photo.each do |wine|
         @all_wines << wine
       end
     end
@@ -17,7 +17,7 @@ class WinesController < ApplicationController
     end
 
     if params[:color]
-      @wines = @all_wines.select { |wine| wine.color == params[:color]}
+      @wines = @all_wines.select { |wine| wine.color == params[:color] }
     else
       @wines = @all_wines
     end
@@ -27,6 +27,5 @@ class WinesController < ApplicationController
   def show
     @wine = Wine.find(params[:id])
     @recipe = Recipe.find(params[:recipe_id]) if params[:recipe_id]
-
   end
 end
